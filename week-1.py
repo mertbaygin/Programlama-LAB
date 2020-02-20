b195c40c50210dfe5afdf216e152a27980e665e6
Mert <48244784+mertbaygin@users.noreply.github.com>
2020-02-20 21:59:43 +0300

    Update week-1.py

--- week-1.py
+++ week-1.py
@@ -8,3 +8,17 @@ def power(a,b):
       return power(a*a,b//2)
     else:
       return power(a*a,b//2)*a
+    
+    *************************************************
+liste=[4,-3,5,-2,-1,2,6,-2]
+max_=0
+for i in range(8):
+  for j in range(i,8):
+    t=0
+    for k in range(i,j):
+      t=t+liste[k]
+    if max_<t:
+      max_=t
+      ibas,json=i,j
+print(max_,ibas,json)
+    
